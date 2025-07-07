4243b51fe5ee3ae308fa1efb0ea7fd057d139537
jvbyrnes <jackvbyrnes@gmail.com>
2025-07-07 16:59:13 +1000

    updated main function to only search for papers and write the json response with proper indentation

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from post_search_request import search_works, search_journals, search_data_providers
+import json
 
 def main():
+    output_file_path="machine-learning-query-results.json"
     works_response = search_works(search_query='machine learning in medicine', limit=1)
-    journals_response = search_journals(search_query="machine learning", limit=1)
-    data_providers_response = search_data_providers(search_query="dataset")
 
-
-    print(works_response)
-    print("-"*60)
-    print(journals_response)
-    print("-"*60)
-    print(data_providers_response)
-
+    with open(output_file_path, 'w') as json_file:
+        json.dump(works_response, json_file, indent=4)
 
 if __name__ == "__main__":
     main()
