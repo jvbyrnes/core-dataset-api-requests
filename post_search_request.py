0d289a530da5a8537f22cb06cd44781d79be894b
jvbyrnes <jackvbyrnes@gmail.com>
2025-07-07 15:34:48 +1000

    created main file showing how to run everything

--- post_search_request.py
+++ post_search_request.py
@@ -20,7 +20,7 @@ def search_core_dataset(entity, search_query, offset, limit):
 
     return response.json()
 
-def search_papers(search_query, offset=0, limit=10):
+def search_works(search_query, offset=0, limit=10):
     return search_core_dataset(entity='works', search_query=search_query, offset=offset, limit=limit)
 
 def search_journals(search_query, offset=0, limit=10):
